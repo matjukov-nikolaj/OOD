7288ff91a955326b78f85719910c7e46921ecd30
matjukov-nikolaj <blatnoi.kolyan@yandex.ru>
2019-03-24 15:03:50 +0300

    added skeleton for streams

---
+++ lw03/streams/src/main/java/com/ood/App.java
@@ -0,0 +1,13 @@
+package com.ood;
+
+import org.apache.log4j.Logger;
+
+public class App
+{
+    private static final Logger LOG = Logger.getLogger(App.class);
+
+    public static void main( String[] args )
+    {
+        LOG.info("Hello world!");
+    }
+}
